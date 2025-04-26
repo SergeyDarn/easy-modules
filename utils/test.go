dbc57a1e6a342c19938d994975a809097f4e48dd
Sergey Darn <sergeydarn@gmail.com>
2025-04-26 06:14:58 +0300

    test: add helper for checking errors

--- utils/test.go
+++ utils/test.go
@@ -13,3 +13,9 @@ func TestPanic(t *testing.T, testName string, functionToTest func()) {
 
 	t.Errorf(PrepareDangerOutput("Expected test %s to error."), testName)
 }
+
+func CheckTestError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
